fae28a643e055b3fe6f8d1ffa2550a5dca24ccc4
Patrick Maweu <muilipato@gmail.com>
2023-05-03 12:28:10 +0300

    Botcard updated

--- bot-batllr/src/components/DisplayBot.js
+++ bot-batllr/src/components/DisplayBot.js
@@ -3,7 +3,8 @@ function DisplayBot({bot}){
     return(
         <div className="single-bot">
             <img className='bot-image' src={bot.avatar_url}/>
-            <p>Name: {bot.name}</p>
+            <p><strong>Name: {bot.name}</strong></p>
+            <p>{bot.catchphrase}</p>
             <p>Health: {bot.health}</p>
             <p>Damage: {bot.damage}</p>
             <p>Armour: {bot.armor}</p>
